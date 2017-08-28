16ad915335961b3f618f46f400f4a68c238d3d55
Roni Ahola <roni.ahola@student.tut.fi>
2017-08-28 10:37:35 +0300

    mm

---
+++ bitcointalk/parse_msgs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 17:52:48 2017
+
+@author: roni
+"""
+from bs4 import BeatifulSoup
---
+++ poloniex/randomdata.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+def RandomData(dimensions):
+    """ Returns random data for test purposes
+        # Dimensions eg. (1200,1)
+    """
+    import numpy as np
+    data = np.zeros(dimensions)
+    
